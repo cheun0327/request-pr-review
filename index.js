696531fae2c080d5cf02a4f98cc0eb01b6bd9fbf
triple-lucy <48849608+cheun0327@users.noreply.github.com>
2025-02-23 00:42:25 +0900

    Update index.js

--- index.js
+++ index.js
@@ -108,8 +108,8 @@ const refineToApiUrl = repoUrl => {
 
             // PR 목록 가져오기
             const pulls = await authFetch(`${BASE_API_URL}/pulls`);
-            core.info(`Found ${pulls.length} PRs for ${repoUrl}`);
-            core.info(`Repo Name: ${repoUrl.replace(/\/$/, "").split("/").slice(-1)[0]}`);
+            core.info(`Found ${pulls.length} PRs for ${core.getInput("repoUrl")}`);
+            core.info(`Repo Name: ${core.getInput("repoUrl").replace(/\/$/, "").split("/").slice(-1)[0]}`);
 
             // PR 목록을 저장
             allPRs = allPRs.concat(
